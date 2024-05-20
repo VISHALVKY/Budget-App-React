6e0a45c06083789d8fd5e9509a580cab9c6df239
VISHALVKY <vishalvky1510@gmail.com>
2024-05-20 16:59:53 +0530

    Updates

---
+++ precache-manifest.b759d51426edf84461f23e79cea933c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e55619fc9a4d52ab8e81e72880ec401",
+    "url": "/Budget-App-React/index.html"
+  },
+  {
+    "revision": "2633210a928497d44cbc",
+    "url": "/Budget-App-React/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2633210a928497d44cbc",
+    "url": "/Budget-App-React/static/js/2.360a4eba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget-App-React/static/js/2.360a4eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0699b9f77680ad9053ac",
+    "url": "/Budget-App-React/static/js/main.1530d9b0.chunk.js"
+  },
+  {
+    "revision": "27c89d1be5bf0c7fcf11",
+    "url": "/Budget-App-React/static/js/runtime-main.dbc98dd1.js"
+  }
+]);
